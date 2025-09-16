7f47b7759633617f2f5805f61b6324db1637505d
Adriano de Medeiros <adrianomsousa@gmail.com>
2025-09-16 18:01:56 -0300

    babel installation and configuration

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,18 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use:
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
             },
         ],
     },
